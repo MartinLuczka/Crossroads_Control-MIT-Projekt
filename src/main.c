7eefa7221ae5e6ea6b25d6b55b0087b5861ac934
MartinLuczka <luczka.martin@gmail.com>
2024-06-03 00:24:18 +0200

    Procvičování vstupů a výstupů

--- src/main.c
+++ src/main.c
@@ -4,40 +4,44 @@
 #include "main.h"
 #include "milis.h"
 //#include "delay.h"
-//#include "uart1.h"
 
 void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
 
     GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-#if defined (BTN_PORT) || defined (BTN_PIN)
     GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
-#endif
+    GPIO_Init(GPIOB, GPIO_PIN_5, GPIO_MODE_OUT_PP_LOW_SLOW);
+    GPIO_Init(GPIOB,GPIO_PIN_2,GPIO_MODE_IN_PU_NO_IT);
 
     init_milis();
-    //init_uart1();
 }
 
 
 int main(void)
 {
-  
-    uint32_t time = 0;
-
     init();
 
-    while (1) {
-#if defined (BTN_PORT) && defined (BTN_PIN)
-        if (milis() - time > 333 && !PUSH(BTN)) {
-#else
-        if (milis() - time > 333 ) {
-#endif
-            REVERSE(LED); 
+    uint32_t time = 0;
+
+    bool aktualni_stav_tlacitka;
+
+    while(1) {
+        if(milis() - time > 100) {
             time = milis();
-            //printf("%ld\n", time);
+            if (GPIO_ReadInputPin(GPIOB,GPIO_PIN_2) == RESET) {
+                aktualni_stav_tlacitka = 1;
+            }
+            else {
+                aktualni_stav_tlacitka = 0;
+            }
+            if (aktualni_stav_tlacitka == 1) {
+                GPIO_WriteLow(GPIOB, GPIO_PIN_5);
+            }
+            else {
+                GPIO_WriteHigh(GPIOB, GPIO_PIN_5);
+            }
         }
-        //delay_ms(333);
     }
 }
 
